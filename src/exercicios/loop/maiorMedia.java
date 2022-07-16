324e1ed9a60be4216e69b0833e91cdd49462696e
daniperino <daniperino@gmail.com>
2022-07-16 20:58:49 -0300

    incluir exercício média

--- src/exercicios/loop/maiorMedia.java
+++ src/exercicios/loop/maiorMedia.java
@@ -12,6 +12,29 @@ public class maiorMedia {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        double maior = 0;
+        double media;
+        double soma = 0;
+
+        int count = 0; //contador
+        do {
+            System.out.println("Número: ");
+            double numero = scan.nextDouble();
+
+            soma = soma + numero;
+
+            if(numero > maior) maior = numero;
+            count = count +1;
+
+        }while (count < 5);
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Media: " + (soma/5));
+
+
+
+
+
 
     }
 }
